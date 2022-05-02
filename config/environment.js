e63420775c66c5355e6c9faa1eca917973d3dbb8
N1x9d <nsh19021999@yandex.ru>
2022-05-02 14:22:08 +0500

    fix2

--- config/environment.js
+++ config/environment.js
@@ -29,6 +29,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    
   }
 
   if (environment === 'test') {
@@ -45,6 +46,11 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+  }
+
+  if (environment === 'ci') {
+    // here you can enable a production-specific feature
+    ENV.rootURL='/BooksDemo/';
   }
 
   return ENV;
