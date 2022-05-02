dbd45380df1a4b1d479f5da90981bd98d65ff79e
N1x9d <nsh19021999@yandex.ru>
2022-05-02 14:19:02 +0500

    fix

---
+++ app/helpers/env.js
@@ -0,0 +1,10 @@
+//import { get } from '@ember/object';
+import { helper } from '@ember/component/helper';
+//import  ENV  from './config/environment';
+
+
+export function env(params/*, hash*/) {
+  return params;
+}
+
+export default helper(env);
