93263b15224973e338f276c35ad64ec595669f22
N1x9d <nsh19021999@yandex.ru>
2022-05-11 12:28:05 +0500

    addEdit and create

--- app/services/data.js
+++ app/services/data.js
@@ -5,14 +5,61 @@ export default Service.extend({
     getDataSpeackers(){
        return fetch(`${ENV.backEndURL}/speackers`).then((response)=> response.json())        
     },
-    getDataBooks(){
-        return fetch(`${ENV.backEndURL}/books`).then((response)=> response.json())        
+    getDataBooks(search){
+        let quertParams='';
+        if(search){
+            quertParams =`?q=${search}`;
+        }
+        return fetch(`${ENV.backEndURL}/books${quertParams}`).then((response)=> response.json())        
     },
 
     getBook(id){
         return fetch(`${ENV.backEndURL}/books/${id}`).then((response)=> response.json())    
     },
-    deliteBook(author){
-        return fetch(`${ENV.backEndURL}/books/${author.id}`,{method: 'DELETE'});
+    deliteBook(id){
+        return fetch(`${ENV.backEndURL}/books/${id}`,{method: 'DELETE'});
+    },
+    createBook(book){
+        return fetch(`${ENV.backEndURL}/books`,{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(book)
+        });
+    },
+    updateBook(book){
+        return fetch(`${ENV.backEndURL}/books/${book.id}`,{
+            method: 'PATCH',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(book)
+        });
+    },
+    getSpeacker(id){
+        return fetch(`${ENV.backEndURL}/speackers/${id}`).then((response)=> response.json())    
+    },
+    updateSpeacker(speacker){
+        return fetch(`${ENV.backEndURL}/speackers/${speacker.id}`,{
+            method: 'PATCH',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(speacker)
+        });
+    },
+    deliteSpeacker(id){
+        return fetch(`${ENV.backEndURL}/speackers/${id}`,{method: 'DELETE'});
+    },
+    createSpeacker(speacker){
+        return fetch(`${ENV.backEndURL}/speackers`,{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(speacker)
+        });
     }
+    
 });
