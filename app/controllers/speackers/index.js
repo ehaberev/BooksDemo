d5288b18773b9f1365826fdd144229f73a9a2003
N1x9d <nsh19021999@yandex.ru>
2022-05-12 19:10:04 +0500

    edit and search features

---
+++ app/controllers/speackers/index.js
@@ -0,0 +1,21 @@
+//done
+import Controller from '@ember/controller';
+//import {inject as service} from "@ember/service";
+
+
+export default Controller.extend({
+    queryParams: ["search"],
+    search:'',
+    // actions: {
+    //     searchSp(searchT){
+    //         alert(searchT);
+    //         this.get('search').set('searchT');
+    //         alert(this.get('search'));
+    //    },
+    //    refresh(){
+    //     this.refresh();
+    //     }
+    // }
+
+});
+
