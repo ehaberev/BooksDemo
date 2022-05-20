5e95a0be51ce3967bddbcecb779068fe1cfab52a
Evgeniy Khaberev <ehaberev@yandex.ru>
2022-05-20 15:48:40 +0500

    Add delete speaker ability

--- app/components/speackers-item.js
+++ app/components/speackers-item.js
@@ -1,14 +1,9 @@
 import Component from '@ember/component';
-import {inject as service} from "@ember/service";
 
 export default Component.extend({
-    dataService: service('data'),
     actions:{
-       async deliteSpeacker(id){
-            //console.log(this);
-            await this.get('dataService').deliteSpeacker(id);
-            this.transitionToRoute('speackers');
-            
+        deleteSpeacker(id) {
+          this.get('deleteSpeacker')(id);
         }
     }
 });
--- app/controllers/speackers.js
+++ app/controllers/speackers.js
@@ -1,6 +1,17 @@
 import Controller from '@ember/controller';
+import {inject as service} from "@ember/service";
 
 export default Controller.extend({
+    dataService: service('data'),
     queryParams: ["search"],
     search:'',
+
+    actions: {
+      async deleteSpeacker(id) {
+          const speackerToDelete = this.get('store').peekRecord('speacker', id);
+          if (speackerToDelete) {
+            speackerToDelete.destroyRecord();
+          }
+      }
+    }
 });
